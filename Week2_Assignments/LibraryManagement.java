8d1a8bbb5fc8a526d02a136555a1a59a5151d3b2
KrishnaveniAnanth <veni3093@gmail.com>
2025-07-27 18:05:05 +0530

    Add files via upload

---
+++ Week2_Assignments/LibraryManagement.java
@@ -0,0 +1,15 @@
+package week2.day1_Assignments;
+
+public class LibraryManagement {
+
+	public static void main(String[] args) {
+		// create object
+		Library book = new Library();
+		// call the methods in the class named Library
+		String bookTitle = book.addBook("Effective Java");
+		System.out.println("Bookname:" + bookTitle);
+		book.issueBook();
+
+	}
+
+}
