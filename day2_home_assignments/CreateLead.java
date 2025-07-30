9d62ae8b6299bb9b0cd6f74789a53821039dbe43
KrishnaveniAnanth <veni3093@gmail.com>
2025-07-30 19:47:26 +0530

    Add files via upload
    week2_day2

---
+++ day2_home_assignments/CreateLead.java
@@ -0,0 +1,61 @@
+package week2.day2_home_assignments;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+public class CreateLead {
+
+	public static void main(String[] args) {
+
+		ChromeOptions options = new ChromeOptions();
+
+		options.addArguments("guest");
+
+		// Launch the browser
+		ChromeDriver driver = new ChromeDriver(options);
+		// EdgeDriver driver=new EdgeDriver();
+
+		// Load the url - get
+		driver.get("http://leaftaps.com/opentaps/control/main");
+
+		// Maximize the browser
+		driver.manage().window().maximize();
+
+		driver.findElement(By.id("username")).sendKeys("DemoCSR");
+
+		// Enter the password
+		driver.findElement(By.id("password")).sendKeys("crmsfa");
+
+		// Click the Login button
+		driver.findElement(By.className("decorativeSubmit")).click();
+
+		// Click on the CRMSFA link
+		driver.findElement(By.partialLinkText("CRM")).click();
+
+		// Click on the Leads link
+		driver.findElement(By.linkText("Leads")).click();
+
+		// Click on the Create Lead link
+		driver.findElement(By.linkText("Create Lead")).click();
+
+		// Enter company name
+		driver.findElement(By.id("createLeadForm_companyName")).sendKeys("TestLeaf");
+
+		// Enter first name
+		driver.findElement(By.id("createLeadForm_firstName")).sendKeys("Krishnaveni");
+
+		// Enter the last name
+		driver.findElement(By.id("createLeadForm_lastName")).sendKeys("Ananth");
+
+		// Enter the title
+		driver.findElement(By.id("createLeadForm_generalProfTitle")).sendKeys("Automation");
+
+		// Click on the Create lead button
+		driver.findElement(By.name("submitButton")).click();
+
+		// Close the browser
+		driver.quit();
+	}
+
+}
